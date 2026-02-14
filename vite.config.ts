0e2d33de2e1b4904b1d7422449d0ccb620b46f25
kalmigs <kmigallos@gmail.com>
2026-02-14 23:00:54 +0800

    feat: add vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_ACTIONS ? '/music-tools/' : '/',
-  plugins: [tanstackRouter({ target: 'react', autoCodeSplitting: true }), react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    react(),
+    tailwindcss(),
+    VitePWA({ registerType: 'autoUpdate' }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
